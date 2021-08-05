6147892c4ce536b415181991d849237097bcf43d
Harit Krishan <45740262+robokrishan@users.noreply.github.com>
2021-08-05 17:25:06 +0200

    driver script

---
+++ quad_test.ino
@@ -0,0 +1,33 @@
+//#include <HTTPClient.h>
+//#include <WiFi.h>
+#include <Alpha.h>
+
+Alpha copter;
+
+const char* ssid = "Hasenhoeft";
+const char* pwd = "21162010819665839679";
+const char* pot_server = "http://192.168.178.31/pot";
+
+void setup() {
+  // put your setup code here, to run once:
+  copter.setESCPins(16, 17, 18, 19);
+  copter.attachESC();
+  copter.setPOTPin(34);
+  copter.setLCDAddress(39);
+  copter.setLCDDimensions(16, 2);
+  copter.beginLCD();
+  copter.setPOTIP((char*)pot_server);
+  copter.setSSID((char*)ssid);
+  copter.setPWD((char*)pwd);
+  copter.connect();
+  copter.beginBLDC();
+  copter.updateLCDRC();
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  copter.updateLCDRC();
+  copter.fly();
+}
